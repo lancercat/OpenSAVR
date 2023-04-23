07fcdd9e8ff68e0f5a83e5e8fe893bf39f7e9f66
lasercat <lasercat@gmx.us>
2023-04-23 15:20:57 +0800

    Training scripts

---
+++ neko_2022_soai_zero/project283/close_basemodelXL512_prec_shufIN3_g3kDPs_cycanof_aa_p3/train.py
@@ -0,0 +1,23 @@
+from neko_sdk.MJT.neko_abstract_jtr import neko_modular_joint_training_semipara
+from configs import dan_single_model_train_cfg
+from neko_sdk.environment.root import find_data_root;
+
+import sys
+
+if __name__ == '__main__':
+    if(len(sys.argv)>1):
+        bs=int(sys.argv[1]);
+    else:
+        bs=64
+    trainer=neko_modular_joint_training_semipara(
+        dan_single_model_train_cfg(
+            "jtrmodels",
+            find_data_root(),
+            "../logs/",
+            200,
+            bsize=bs,
+            itrk="Top Nep"
+        )
+    );
+    # with torch.autograd.detect_anomaly():
+    trainer.train(None,flag=False);
